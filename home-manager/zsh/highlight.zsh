c5a50e38344cc319f5c7450756d0eb9182c742b8
ModerNews <polski.gruzin.biz@gmail.com>
2026-08-17 16:50:09 +0200

    colorschemes v3

--- home-manager/zsh/highlight.zsh
+++ home-manager/zsh/highlight.zsh
@@ -3,8 +3,8 @@ if (( ${+FAST_HIGHLIGHT_STYLES} )); then
         red    "#d83e4a"
         orange "#f36630"
         green  "#86a87e"
-        yellow "#ffcc33"
-        navy   "#1a2a53"
+        amber  "#ec8509"
+        blue   "#4c6a84"
         gray   "#666666"
     )
 
@@ -17,17 +17,18 @@ if (( ${+FAST_HIGHLIGHT_STYLES} )); then
         FAST_HIGHLIGHT_STYLES[$_k]="fg=${_fsh_pal[green]}"
     done
 
-    FAST_HIGHLIGHT_STYLES[bracket-level-1]="fg=${_fsh_pal[yellow]},bold"
-    FAST_HIGHLIGHT_STYLES[bracket-level-2]="fg=${_fsh_pal[navy]},bold"
+    FAST_HIGHLIGHT_STYLES[bracket-level-1]="fg=${_fsh_pal[amber]},bold"
+    FAST_HIGHLIGHT_STYLES[bracket-level-2]="fg=${_fsh_pal[blue]},bold"
     FAST_HIGHLIGHT_STYLES[bracket-level-3]="fg=${_fsh_pal[gray]},bold"
-    FAST_HIGHLIGHT_STYLES[single-sq-bracket]="fg=${_fsh_pal[yellow]}"
-    FAST_HIGHLIGHT_STYLES[double-sq-bracket]="fg=${_fsh_pal[yellow]}"
-    FAST_HIGHLIGHT_STYLES[double-paren]="fg=${_fsh_pal[yellow]}"
-    FAST_HIGHLIGHT_STYLES[assign-array-bracket]="fg=${_fsh_pal[yellow]}"
+    FAST_HIGHLIGHT_STYLES[single-sq-bracket]="fg=${_fsh_pal[amber]}"
+    FAST_HIGHLIGHT_STYLES[double-sq-bracket]="fg=${_fsh_pal[amber]}"
+    FAST_HIGHLIGHT_STYLES[double-paren]="fg=${_fsh_pal[amber]}"
+    FAST_HIGHLIGHT_STYLES[assign-array-bracket]="fg=${_fsh_pal[amber]}"
 
-    FAST_HIGHLIGHT_STYLES[single-hyphen-option]="fg=${_fsh_pal[navy]}"
-    FAST_HIGHLIGHT_STYLES[double-hyphen-option]="fg=${_fsh_pal[navy]}"
+    FAST_HIGHLIGHT_STYLES[single-hyphen-option]="fg=${_fsh_pal[blue]}"
+    FAST_HIGHLIGHT_STYLES[double-hyphen-option]="fg=${_fsh_pal[blue]}"
     FAST_HIGHLIGHT_STYLES[unknown-token]="fg=${_fsh_pal[orange]},bold"
+    FAST_HIGHLIGHT_STYLES[variable]="fg=${_fsh_pal[blue]}"
     FAST_HIGHLIGHT_STYLES[comment]="fg=${_fsh_pal[gray]}"
     FAST_HIGHLIGHT_STYLES[path]="none"
     FAST_HIGHLIGHT_STYLES[path-to-dir]="none,underline"
